0750d7186936906c9659e825f1c3e98d70579c0d
Maksim <nikleft12@gmail.com>
2017-11-07 16:25:10 +0200

    Rewrite init.sh (nothing special)

--- etc/init.sh
+++ etc/init.sh
@@ -1,3 +1,3 @@
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/nginx.conf
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo sudo /etc/init.d/nginx restart
