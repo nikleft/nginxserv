0a26b3e100d52cc251ad207cc4977c3b23d6b2bf
Maksim <nikleft12@gmail.com>
2017-11-08 14:55:32 +0200

    Add logic in hello.py |Nginx nothing special

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
-def query_app(environ, start_response)
+def wsgi_application(environ, start_response)
 	#logic
 	status = '200 OK'
 	headers = [ ('Content-Type','text/plain')]
-	body = 'Hello World!'
+	body = [bytes(i+'\n','ascii') for i in environ['QUERY_STRING'].split('&')]
 	start_response(status,headers)
-	return [body]
+	return body
