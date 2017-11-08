2792790507c7ab8028b416e2d63883d9f4b4e619
Maksim <nikleft12@gmail.com>
2017-11-08 15:11:31 +0200

    Rewrite gunicorn boot parameters

--- etc/init.sh
+++ etc/init.sh
@@ -1,5 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
+gunicorn -c hello.py hello:application
+curl -l http://127.0.0.1/hello/
