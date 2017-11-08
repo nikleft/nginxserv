4bd7b425b08ec3269a7f41aa357468ccf5a4fb59
Maksim <nikleft12@gmail.com>
2017-11-08 16:20:46 +0200

    Change proxy ip Nginx (and something else)

--- etc/init.sh
+++ etc/init.sh
@@ -1,5 +1,7 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo sudo /etc/init.d/nginx restart
-gunicorn -c hello.py hello:application
-curl -l http://127.0.0.1/hello/
+sudo /etc/init.d/gunicorn restart
+sudo gunicorn -c hello.py hello:app
+
+#curl -l http://0.0.0.0/8080
