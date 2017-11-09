b0ea8f48abbda9dbca2ceb082cc2f74109536ff5
Maksim <nikleft12@gmail.com>
2017-11-09 16:00:27 +0200

    Changed function name and logic

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
-def wsgi_application(environ, start_response)
+def wsgiapp(environ, start_response):
 	#logic
 	status = '200 OK'
 	headers = [ ('Content-Type','text/plain')]
-	body = [bytes(i+'\n','ascii') for i in environ['QUERY_STRING'].split('&')]
+	body =[str(i)+'\n' for i in environ['QUERY_STRING'].split("&")]
 	start_response(status,headers)
 	return body
+#bytes(i+'\n','ascii')
+#body = [bytes((i+'\n','ascii')) for i in environ['QUERY_STRING'].split('&')]
