2a072b238eb85423754453ec129ae647d2ce7bf3
Maksim <nikleft12@gmail.com>
2017-11-09 15:59:22 +0200

    Changed gunicorn boot command

--- etc/init.sh
+++ etc/init.sh
@@ -2,6 +2,4 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo sudo /etc/init.d/nginx restart
 sudo /etc/init.d/gunicorn restart
-sudo gunicorn -c hello.py hello:app
-
-#curl -l http://0.0.0.0/8080
+sudo gunicorn -c hello.py hello222:wsgiapp
