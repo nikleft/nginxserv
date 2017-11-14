9859eeac657885714c98105abd14f8d74aade1d5
Maksim <nikleft12@gmail.com>
2017-11-14 15:31:56 +0200

    Add gunicorn boot of django app

--- etc/init.sh
+++ etc/init.sh
@@ -2,4 +2,5 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo sudo /etc/init.d/nginx restart
 sudo /etc/init.d/gunicorn restart
-sudo gunicorn -c hello.py hello222:wsgiapp
+#sudo gunicorn -c hello.py hello222:wsgiapp
+sudo gunicorn -c djresp.py viewresp:test
