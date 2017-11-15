c383f0a1ec7be099b2c0e0c555d4ee972d0ec53f
Maksim <nikleft12@gmail.com>
2017-11-15 16:39:55 +0200

    Added default_content_type setting

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render
-
+import os
 from django.http import HttpResponse
+from django.conf import settings
+#os.environ['DJANGO_SETTINGS_MODULE']='ask.settings'
 
+settings.configure(DEFAULT_CONTENT_TYPE='text/html')
 def test(request,*args,**kwargs):
 	return HttpResponse('OK')
 
